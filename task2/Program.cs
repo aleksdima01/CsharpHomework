cd37e50f82fe7c158913d45411aae34c7ccd226b
Dmitrii <aleksdima@mail.ru>
2022-11-24 16:57:59 +0300

    Task2 complete

--- task2/Program.cs
+++ task2/Program.cs
@@ -1,2 +1,11 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.Clear();
+Console.WriteLine("Введите число 1");
+int N1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите число 2");
+int N2 = Convert.ToInt32(Console.ReadLine());
+int max=0;
+if (N1>N2)
+max=N1;
+else 
+max=N2;
+Console.WriteLine(max);
