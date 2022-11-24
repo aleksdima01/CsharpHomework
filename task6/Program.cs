a4b4bf386916a886eff030492fd361287c07eb88
Dmitrii <aleksdima@mail.ru>
2022-11-24 19:56:56 +0300

    Task6 complete

---
+++ task6/Program.cs
@@ -0,0 +1,7 @@
+﻿Console.Clear();
+Console.WriteLine("Введите число:");
+int N1 = Convert.ToInt32(Console.ReadLine());
+if (N1 % 2 != 0)
+    Console.WriteLine("Не четное");
+else
+    Console.WriteLine("Четное");
