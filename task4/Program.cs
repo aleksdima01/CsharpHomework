52fd52dfa9a60324b1bc2a626f9ae61d5b6d2089
Dmitrii <aleksdima@mail.ru>
2022-11-24 19:43:40 +0300

    Task4 complete

--- task4/Program.cs
+++ task4/Program.cs
@@ -3,9 +3,11 @@ Console.WriteLine("Введите число 1");
 int N1 = Convert.ToInt32(Console.ReadLine());
 Console.WriteLine("Введите число 2");
 int N2 = Convert.ToInt32(Console.ReadLine());
-int max = 0;
-if (N1 > N2)
-    max = N1;
-else
+Console.WriteLine("Введите число 3");
+int N3 = Convert.ToInt32(Console.ReadLine());
+int max = N1;
+if (N2 > max)
     max = N2;
+if (N3 > max)
+    max = N3;
 Console.WriteLine(max);
