dca5a26dd3454f542e2eabb45006af719e58ba60
Dmitrii <aleksdima@mail.ru>
2022-11-24 22:45:24 +0300

    Complete task8

---
+++ task8/Program.cs
@@ -0,0 +1,17 @@
+﻿Console.Clear();
+int r = 0;
+Console.WriteLine("Введите число от 1 до N:");
+int N1 = Convert.ToInt32(Console.ReadLine());
+while (N1 < 1)
+{
+    Console.Write("Вы ошиблись!\nВведите верное число:");
+    N1 = Convert.ToInt32(Console.ReadLine());
+}
+for (int i = 1; i <= N1; i++)
+{
+    if (i % 2 == 0)
+    {
+        r = i;
+        Console.Write(r + " ");
+    }
+}
