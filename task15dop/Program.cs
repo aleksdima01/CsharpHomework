30bec56bdca78fb12ead4889b2c6dd02a37e8ece
Dmitrii <aleksdima@mail.ru>
2022-11-28 21:19:47 +0300

    Complete task 15dop

--- task15dop/Program.cs
+++ task15dop/Program.cs
@@ -1,39 +1,16 @@
-﻿//Console.Clear();
-Console.WriteLine("Введите последовательность чисел:");
-int N1 = Convert.ToInt32(Console.ReadLine());
-while (N1<1)
+﻿Console.Clear();
+Console.WriteLine("Введите последовательность чисел(в конце 0):");
+int n = Convert.ToInt32(Console.ReadLine());
+int max1 = n, max2 = 0;
+while (n != 0)
 {
-    Console.Write("Вы ошиблись!\nВведите правильную последовательность:");
-    N1 = Convert.ToInt32(Console.ReadLine());
-}
-int count = 0;
-int count2 = 0;
-int lastel = 0;
-int N2 = 0;
-while (N1>0) 
-{
-count++;
-   N1 /= 10;
-    lastel = N1 / 10;
-    if (lastel % 10 == 0)
+    n = Convert.ToInt32(Console.ReadLine());
+    if (n > max1)
     {
-        N2 = lastel;
-        break;
+        max2 = max1;
+        max1 = n;
     }
+    else if (n > max2)
+        max2 = n;
 }
-//Console.WriteLine($"Количество цифр вашего числа:{count}");
-Console.WriteLine($"lastel:{lastel}");
-Console.WriteLine($"lastel:{N2}");
-
-while (N2 > 0)
-{
-    count2++;
-    N2 /= 10;
-    Console.WriteLine($"Количество цифр вашего числа:{count2}");
-}
- Console.WriteLine($"Количество цифр вашего числа:{count2}");
-int n = 0;
-for (n=count2; n > 0;n--)
-{
-
-}
+Console.WriteLine($"Второе по величине число:{max2}");
