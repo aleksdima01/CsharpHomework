78e6a4ffa2e36c29b344a4f981e004b172fab77e
Dmitrii <aleksdima@mail.ru>
2022-11-28 16:23:57 +0300

    Complete task 15

---
+++ task15/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Clear();
+Console.WriteLine("Введите день недели 1-7:");
+int N1 = Convert.ToInt32(Console.ReadLine());
+while (N1<1 || N1>7)
+{Console.Write("Вы ошиблись! Такого дня недели не существует!\nВведите верный день недели:");
+    N1 = Convert.ToInt32(Console.ReadLine());
+}
+
+if (N1>0&&N1<6)
+Console.WriteLine("Будний день!");
+else 
+Console.WriteLine("Выходной!");
