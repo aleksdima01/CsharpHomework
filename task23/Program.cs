6b177fbd9264e6e7dea874f58e80758dca0b0a9c
Dmitrii <aleksdima@mail.ru>
2022-12-01 11:35:08 +0300

    Complete task 23

---
+++ task23/Program.cs
@@ -0,0 +1,11 @@
+﻿//Таблица кубов введенного числа
+Console.Clear();
+Console.WriteLine("Введите число:");
+int N = Convert.ToInt32(Console.ReadLine());
+int result = 0;
+ Console.WriteLine("Ваша таблица кубов:");
+for (int i=1; i <= N; i++)
+{
+    result = (int)Math.Pow(i, 3);
+    Console.WriteLine(result);
+}
