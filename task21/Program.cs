2f2433c602190094975d9c39744bf6040bb4b924
Dmitrii <aleksdima@mail.ru>
2022-12-01 11:05:10 +0300

    Complete task 21

---
+++ task21/Program.cs
@@ -0,0 +1,17 @@
+﻿Console.Clear();
+Console.WriteLine("Введите координату точки Ax");
+double Ax = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("Введите координату точки Ay");
+double Ay = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("Введите координату точки Az");
+double Az = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("Введите координату точки Yx");
+double Yx = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("Введите координату точки Yy");
+double Yy = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("Введите координату точки Yz");
+double Yz = Convert.ToDouble(Console.ReadLine());
+
+double result = 0;
+result = Math.Sqrt(Math.Pow(Ax - Yx,2) + Math.Pow(Ay - Yy,2) + Math.Pow(Az - Yz,2));
+Console.WriteLine($"Расстояние между точками:{result}");
