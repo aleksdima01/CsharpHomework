437140289eee8061c6b2566616ae54e59f1b179c
Dmitrii <aleksdima@mail.ru>
2022-12-01 21:42:23 +0300

    Complete task 23 dop

--- task23dop/Program.cs
+++ task23dop/Program.cs
@@ -1,17 +1,25 @@
 ﻿Console.Clear();
- Console.WriteLine("Введите количество кустов на грядке:");
- int N1 = Convert.ToInt32(Console.ReadLine());
-int i =0;
-while (i<N1)
+Console.WriteLine("Введите количество кустов на грядке:");
+int N1 = Convert.ToInt32(Console.ReadLine());
+int i = 0;
+int result = 0;
+int[] numbers = new int[N1];
+while (i < N1)
 {
-    Console.WriteLine("Введите количество ягод на кусте:");
-int N2 = Convert.ToInt32(Console.ReadLine());
- int[] numbers=new int[1000];
-for (int j = 0; j <N1; j++)
-{
-    numbers[j]=N2;
-    Console.WriteLine(numbers[j]);
+    Console.WriteLine($"Введите количество ягод на {i + 1} кусте:");
+    int N2 = Convert.ToInt32(Console.ReadLine());
+    numbers[i] = N2;
+    i++;
 }
- }
 
-
+//Console.WriteLine(" ");
+for (i = 1; i < numbers.Length - 1; i++)
+{
+    if (result < numbers[i - 1] + numbers[i] + numbers[i + 1])
+        result = numbers[i - 1] + numbers[i] + numbers[i + 1];
+}
+if (result < numbers[N1 - 1] + numbers[N1 - 2] + numbers[0])
+    result = numbers[N1 - 1] + numbers[N1 - 2] + numbers[0];
+if (result < numbers[0] + numbers[1] + numbers[N1 - 1])
+    result = numbers[0] + numbers[1] + numbers[N1 - 1];
+Console.WriteLine(result);
