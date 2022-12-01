2f271bfdc4dd145b6e20a11d30f14090234cb782
Dmitrii <aleksdima@mail.ru>
2022-12-01 17:33:50 +0300

    task 23 dop in progress

---
+++ task23dop/Program.cs
@@ -0,0 +1,17 @@
+﻿Console.Clear();
+ Console.WriteLine("Введите количество кустов на грядке:");
+ int N1 = Convert.ToInt32(Console.ReadLine());
+int i =0;
+while (i<N1)
+{
+    Console.WriteLine("Введите количество ягод на кусте:");
+int N2 = Convert.ToInt32(Console.ReadLine());
+ int[] numbers=new int[1000];
+for (int j = 0; j <N1; j++)
+{
+    numbers[j]=N2;
+    Console.WriteLine(numbers[j]);
+}
+ }
+
+
