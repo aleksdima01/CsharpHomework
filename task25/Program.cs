f46a872fb21b1261add8e4f2abc7837f75dc410d
Dmitrii <aleksdima@mail.ru>
2022-12-05 14:26:44 +0300

    Complete task 25

---
+++ task25/Program.cs
@@ -0,0 +1,12 @@
+﻿//Возведение числа А в степень В
+Console.Clear();
+Console.WriteLine("Введите число A:");
+int A = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите число B:");
+int B = Convert.ToInt32(Console.ReadLine());
+int result = 1;
+for (int i = 0; i < B; i++)
+{
+    result = result* A;
+}
+Console.WriteLine($"Ваше число {A} в степени {B} =\n{result} ");
