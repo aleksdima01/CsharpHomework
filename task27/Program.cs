d2b130d0f988514f6229bc76532da3dbe4f99e0c
Dmitrii <aleksdima@mail.ru>
2022-12-05 15:47:45 +0300

    task27 in progress

---
+++ task27/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.Clear();
+Console.WriteLine("Введите число:");
+string N1 = Console.ReadLine();
+char[] minus = {'-'};
+ N1 = N1.TrimStart(minus);
+
+int result = 0;
+
+for (int i = 0; i < N1.Length; i++)
+{
+    result = result+N1[i];
+}
+
+
+    Console.WriteLine(int.Parse(result.ToString()));
